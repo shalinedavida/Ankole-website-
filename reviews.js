c63fd1607ec49e7d73698b8b311ab38a9dae8300
Shaline Davida <shalinedavida@gmail.com>
2025-04-18 17:52:14 +0300

    fifth commit

--- reviews.js
+++ reviews.js
@@ -1,3 +1,38 @@
-const previousButton= document.getElementById("previousButton");
-const nextButton= document.getElementById("nextButton");
+const previousButton= document.querySelector(".previousButton");
+
+const nextButton= document.querySelector(".nextButton");
+
+const slider = document.querySelector(".slider");
+
+const testimonial = document.querySelectorAll(".slide");
+
+let slideNumber = 1;
+
+const length= testimonial.length;
+
+const nextSlide= () =>{
+    slider.style.transform =`translateX(-${slideNumber* 800}px)`;
+    slideNumber++;
+};
+
+const prevSlide = () => {
+    slider.style.transform =`translateX(-${(slideNumber-2)* 800}px)`;
+    slideNumber--;
+};
+
+const getFirstSlide = () => {
+    slider.style.transform =`translate(0px)`;
+    slideNumber=1;
+};
+ const getLastSlide = () =>{
+    slider.style.transform =`translateX(-${(length-1)*800}px)`;
+    slideNumber=length;
+ };
+ nextButton.addEventListener("click",()=>{
+    slideNumber < length ? nextSlide() : getFirstSlide();
+ });
+ previousButton.addEventListener("click",() =>{
+    slideNumber > 1 ? prevSlide() :getLastSlide();
+    changeClr()
+ });
 
